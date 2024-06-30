3a35fc1506d71c634261a3182e0e72c88e793186
adibfirman <dib.firman@gmail.com>
2024-06-30 23:49:53 +0700

    fix: double active tsserver and vtsls

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,18 @@ local get_root_dir = function(fname)
   return util.root_pattern(".git")(fname) or util.root_pattern("package.json", "tsconfig.json")(fname)
 end
 
+require("lazyvim.util").lsp.on_attach(function(client)
+  local active_clients = vim.lsp.get_clients()
+  if client.name == "vtsls" then
+    for _, client_ in pairs(active_clients) do
+      -- stop tsserver if vtsls is already active
+      if client_.name == "tsserver" then
+        client_.stop()
+      end
+    end
+  end
+end)
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -16,6 +28,12 @@ return {
         root_dir = get_root_dir,
         format = true,
         quiet = false,
+        settings = {
+          useFlatConfig = true,
+          experimental = {
+            useFlatConfig = nil,
+          },
+        },
       },
       tailwindcss = {
         root_dir = function(fname)
@@ -23,12 +41,30 @@ return {
           return lsp_util.root_pattern("tailwind.config.js", "tailwind.config.cjs", "tailwind.config.mjs")(fname)
         end,
       },
-      vtsls = {
-        root_dir = get_root_dir,
-      },
       tsserver = {
         root_dir = get_root_dir,
       },
+      vtsls = {
+        root_dir = get_root_dir,
+      },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" or client.name == "vtsls" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+      -- vtsls = function()
+      --   require("lazyvim.util").lsp.on_attach(function(client)
+      --     if client.name == "tsserver" then
+      --       client.stop(true)
+      --     end
+      --   end)
+      -- end,
     },
   },
 }
