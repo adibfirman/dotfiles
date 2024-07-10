ac4c855409f3f32490a62a54029e85e530e03387
adibfirman <dib.firman@gmail.com>
2024-07-10 07:44:39 +0700

    change lsp config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,24 +3,6 @@ local get_root_dir = function(fname)
   return util.root_pattern(".git")(fname) or util.root_pattern("package.json", "tsconfig.json")(fname)
 end
 
-local function get_node_version()
-  local handle = io.popen("node -v")
-  local version = handle:read("*a")
-  handle:close()
-  return version
-end
-
-local function is_node_version_greater_than_16()
-  local version = get_node_version()
-  local major_version = tonumber(version:match("v(%d+)"))
-
-  if major_version > 16 then
-    return true
-  else
-    return false
-  end
-end
-
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -28,18 +10,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   opts = {
-    inlay_hints = { enabled = false },
+    -- inlay_hints = { enabled = false },
     servers = {
       eslint = {
         root_dir = get_root_dir,
         format = true,
         quiet = false,
-        settings = {
-          useFlatConfig = true,
-          experimental = {
-            useFlatConfig = nil,
-          },
-        },
       },
       tailwindcss = {
         root_dir = function(fname)
@@ -55,16 +31,36 @@ return {
       },
     },
     setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" or client.name == "vtsls" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
+      -- eslint = function()
+      --   require("lazyvim.util").lsp.on_attach(function(client)
+      --     if client.name == "eslint" then
+      --       client.server_capabilities.documentFormattingProvider = true
+      --     elseif client.name == "tsserver" or client.name == "vtsls" then
+      --       client.server_capabilities.documentFormattingProvider = false
+      --     end
+      --   end)
+      -- end,
       tsserver = function()
+        local function get_node_version()
+          local handle = io.popen("node -v")
+          if not (handle == nil) then
+            local version = handle:read("*a")
+            handle:close()
+            return version
+          end
+        end
+
+        local function is_node_version_greater_than_16()
+          local version = get_node_version()
+          local major_version = tonumber(version:match("v(%d+)"))
+
+          if major_version > 16 then
+            return true
+          else
+            return false
+          end
+        end
+
         require("lazyvim.util").lsp.on_attach(function(client)
           if client.name == "tsserver" then
             if is_node_version_greater_than_16() then
