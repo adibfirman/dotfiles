55f1ee83f462fde6c68ce9a0a6b8456818c3d064
adibfirman <dib.firman@gmail.com>
2024-08-17 19:06:39 +0700

    okay cool

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
 return {
   {
-    -- "sainnhe/everforest",
     "lalitmee/cobalt2.nvim",
-    event = { "ColorSchemePre" }, -- if you want to lazy load
+    event = { "ColorSchemePre" },
     dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
     init = function()
       require("colorbuddy").colorscheme("cobalt2")
     end,
   },
 }
+-- return {
+--   "tokyonight.nvim",
+--   lazy = true,
+--   opts = {
+--     transparent = true,
+--     styles = {
+--       sidebars = "transparent",
+--       floats = "transparent",
+--     },
+--   },
+-- }
