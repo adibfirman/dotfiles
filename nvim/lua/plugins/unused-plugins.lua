91a58ef2208cbb0710ece53a2c539e0d2e20ac35
adibfirman <dib.firman@gmail.com>
2024-08-21 22:52:24 +0700

    modified

---
+++ nvim/lua/plugins/unused-plugins.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+}
