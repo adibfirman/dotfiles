fd3fe97e1b26c315d3abc384ccdc140f49d2d98c
adibfirman <dib.firman@gmail.com>
2024-12-27 22:31:53 +0700

    fix: typo in lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,12 +14,12 @@ return {
     "williamboman/mason.nvim",
     cmd = "Mason",
     build = ":MasonUpdate",
-    config = function(_, opts)
+    setup = function(_, opts)
       require("mason").setup(opts)
       local mr = require("mason-registry")
+
       mr:on("package:install:success", function()
         vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
           require("lazy.core.handler.event").trigger({
             event = "FileType",
             buf = vim.api.nvim_get_current_buf(),
