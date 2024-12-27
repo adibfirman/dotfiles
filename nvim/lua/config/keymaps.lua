f36cd47a4ae1e41887a334aa5f9668abc1a4f21c
adibfirman <dib.firman@gmail.com>
2024-12-27 17:00:12 +0700

    make a dir pretty

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- quit
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
