4e97b3736ddddfd705e79b5c855b6ffe84f157a1
adibfirman <dib.firman@gmail.com>
2024-12-27 16:21:46 +0700

    init ui plugins

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sainnhe/edge",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.edge_enable_italic = true
+      vim.cmd.colorscheme("edge")
+    end,
+  },
+}
