405883a20aa65be3377493b76c2ad5ebad98015d
adibfirman <dib.firman@gmail.com>
2024-12-29 09:39:41 +0700

    feat: adding bufferline

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,42 @@
 return {
   {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        close_command = function(n)
+          ---@diagnostic disable-next-line: undefined-field
+          Snacks.bufdelete(n)
+        end,
+        right_mouse_command = function(n)
+          ---@diagnostic disable-next-line: undefined-field
+          Snacks.bufdelete(n)
+        end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+  {
     "folke/snacks.nvim",
     lazy = false,
     priority = 1000,
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,13 +1,4 @@
 return {
-  -- {
-  --   "sainnhe/edge",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.g.edge_enable_italic = true
-  --     vim.cmd.colorscheme("edge")
-  --   end,
-  -- },
   {
     "catppuccin/nvim",
     name = "catppuccin",
