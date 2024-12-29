ba90c5a78b6f315df15c411afd584dc72c3e4015
adibfirman <dib.firman@gmail.com>
2024-12-29 08:15:27 +0700

    feat: adding cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,6 +49,102 @@ end
 
 return {
   {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+          end,
+        },
+        window = {
+          completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+              cmp.select_next_item()
+            elseif vim.snippet.active({ direction = 1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(1)
+              end)
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.snippet.active({ direction = -1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(-1)
+              end)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        -- mapping = cmp.mapping.preset.insert({
+        --   ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        --   ["<C-Space>"] = cmp.mapping.complete(),
+        --   ["<C-e>"] = cmp.mapping.abort(),
+        --   ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- }),
+        sources = cmp.config.sources({
+          -- { name = "nvim_lsp" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     setup = function()
@@ -147,6 +243,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local mason_lspconfig = require("mason-lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
       -- This will remove buffer permanently if the buffer not longer in the list
       local function buffer_augroup(group, bufnr, cmds)
@@ -158,6 +255,8 @@ return {
           vim.api.nvim_create_autocmd(event, vim.tbl_extend("keep", { group = group, buffer = bufnr }, cmd))
         end
       end
+
+      local capabilities = cmp_nvim_lsp.default_capabilities()
 
       -- attach this on lsp server in params "on_attach" for each lsp
       local function on_attach(client, bufnr)
@@ -173,7 +272,7 @@ return {
         ["eslint"] = function()
           lspconfig["eslint"].setup({
             root_dir = get_root_dir,
-            -- capabilities = capabilities,
+            capabilities = capabilities,
             format = true,
             on_attach = function(client, bufnr)
               on_attach(client, bufnr)
@@ -189,6 +288,7 @@ return {
             filetypes = { "lua" },
             format = true,
             on_attach = on_attach,
+            capabilities = capabilities,
           })
         end,
         ["vtsls"] = function()
@@ -196,7 +296,7 @@ return {
 
           if is_node_16() then
             lspconfig["vtsls"].setup({
-              -- capabilities = capabilities,
+              capabilities = capabilities,
               root_dir = get_root_dir,
               on_attach = on_attach,
               enabled = enabled,
@@ -209,7 +309,7 @@ return {
           if is_node_16() == false then
             -- https://github.com/neovim/nvim-lspconfig/pull/3232
             lspconfig["ts_ls"].setup({
-              -- capabilities = capabilities,
+              capabilities = capabilities,
               root_dir = get_root_dir,
               on_attach = on_attach,
               enabled = enabled,
