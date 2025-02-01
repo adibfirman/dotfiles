4a282f75a467ff88102964ed98a30eb0e4c68cdc
adibfirman <dib.firman@gmail.com>
2025-02-01 09:41:57 +0700

    feat: adding mini ai and surround

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "echasnovski/mini.ai",
+    version = "*",
+    config = function()
+      require("mini.ai").setup()
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
+  {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
