9e9a395479cc42153cb35cac017faf5d29f06966
adibfirman <dib.firman@gmail.com>
2025-07-23 15:23:24 +0700

    filter undo and redo

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -23,6 +23,18 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "before #%d+" },
+              { find = "after #%d+" },
+            },
+          },
+          opts = { skip = true },
+        },
+      },
     })
   end,
 }
