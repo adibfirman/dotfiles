30d04d5a540d2f6d0c2b1477315839a1d75224da
adibfirman <dib.firman@gmail.com>
2025-07-24 15:18:30 +0700

    adjust lsp due to neovim v11

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,49 +12,34 @@ return {
     "saghen/blink.cmp",
   },
   config = function()
-    local lspconfig = require("lspconfig")
     local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    vim.lsp.enable("astro", {
+    local astro = {
       filetypes = { "astro" },
       capabilities = capabilities,
-    })
+    }
 
-    vim.lsp.enable("lua_ls", {
+    local lua_ls = {
       filetypes = { "lua" },
       format = true,
       capabilities = capabilities,
       root_dir = get_base_root_dir,
-    })
+    }
 
-    vim.lsp.enable("ts_ls", function()
-      local data_path = vim.fn.stdpath("data")
-      local vue_lsp_loc = data_path .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-      local format = { "javascript", "typescript", "vue", "typescriptreact", "javascriptreact" }
-
-      lspconfig["ts_ls"].setup({
-        capabilities = capabilities,
-        root_dir = get_base_root_dir,
-        init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = vue_lsp_loc,
-              languages = format,
-            },
-          },
-        },
-        filetypes = format,
-      })
-    end)
-
-    vim.lsp.enable("jsonls", {
+    local ts_ls = {
       capabilities = capabilities,
-    })
+      root_dir = get_base_root_dir,
+    }
 
-    vim.lsp.enable("vue_ls", {
-      filetypes = { "vue" },
+    local jsonls = {
       capabilities = capabilities,
-    })
+    }
+
+    vim.lsp.enable("astro", astro)
+    vim.lsp.enable("jsonls", jsonls)
+    vim.lsp.enable("lua_ls", lua_ls)
+    vim.lsp.enable("ts_ls", ts_ls)
+    vim.lsp.enable("vtsls")
+    vim.lsp.enable("vue_ls")
   end,
 }
