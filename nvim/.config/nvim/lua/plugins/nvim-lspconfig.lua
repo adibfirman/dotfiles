01a87638daf375d8db852e1e2b0d0d5ed04bbcc2
adibfirman <dib.firman@gmail.com>
2025-07-28 14:21:21 +0700

    lsp stuff

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,3 @@
-local get_base_root_dir = function(fname)
-  local util = require("lspconfig.util")
-  return util.root_pattern(".git")(fname) or util.root_pattern("package.json", "tsconfig.json")(fname)
-end
-
 return {
   "neovim/nvim-lspconfig",
   version = "*",
@@ -14,31 +9,14 @@ return {
   config = function()
     local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    local astro = {
-      filetypes = { "astro" },
+    vim.lsp.config("*", {
       capabilities = capabilities,
-    }
+    })
 
-    local lua_ls = {
-      filetypes = { "lua" },
-      format = true,
-      capabilities = capabilities,
-      root_dir = get_base_root_dir,
-    }
-
-    local ts_ls = {
-      capabilities = capabilities,
-      root_dir = get_base_root_dir,
-    }
-
-    local jsonls = {
-      capabilities = capabilities,
-    }
-
-    vim.lsp.enable("astro", astro)
-    vim.lsp.enable("jsonls", jsonls)
-    vim.lsp.enable("lua_ls", lua_ls)
-    vim.lsp.enable("ts_ls", ts_ls)
+    vim.lsp.enable("astro")
+    vim.lsp.enable("jsonls")
+    vim.lsp.enable("lua_ls")
+    vim.lsp.enable("ts_ls")
     vim.lsp.enable("vtsls")
     vim.lsp.enable("vue_ls")
   end,
