4c20a7275640025097715ca56b0d837cb03bbe33
adibfirman <dib.firman@gmail.com>
2025-07-29 16:07:06 +0700

    add custom args cli go

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -3,6 +3,28 @@ return {
   branch = "master",
   config = function()
     local lint = require("lint")
+
+    lint.linters.golangcilint = {
+      args = {
+        "run",
+        "--output.json.path=stdout",
+        -- Overwrite values possibly set in .golangci.yml
+        "--output.text.path=",
+        "--output.tab.path=",
+        "--output.html.path=",
+        "--output.checkstyle.path=",
+        "--output.code-climate.path=",
+        "--output.junit-xml.path=",
+        "--output.teamcity.path=",
+        "--output.sarif.path=",
+        "--issues-exit-code=0",
+        "--show-stats=false",
+        function()
+          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+        end,
+      },
+    }
+
     lint.linters_by_ft = {
       css = { "eslint_d" },
       go = { "golangcilint" },
