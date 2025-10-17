abd4ac24350a9b5aa2734284fc7184456805284e
adibfirman <dib.firman@gmail.com>
2025-10-17 23:04:32 +0700

    feat: setup miss keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,14 @@
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
-vim.keymap.set("v", "<", "<gv", { desc = "Better indent to left" })
-vim.keymap.set("v", ">", ">gv", { desc = "Better indent to right" })
+local set = vim.keymap.set
+
+set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+set("v", "<", "<gv", { desc = "Better indent to left" })
+set("v", ">", ">gv", { desc = "Better indent to right" })
 
 -- notification stuff
-vim.keymap.set("n", "<leader>nh", "<cmd>Notifications<cr>", { desc = "Show history of notification" })
+set("n", "<leader>nh", "<cmd>Notifications<cr>", { desc = "Show history of notification" })
 
 -- Git stuff
--- vim.keymap.set("n", "<leader>gg", "<cmd>:lua Snacks.lazygit.open()<cr>", { desc = "Lazy Git" })
-vim.keymap.set("n", "<leader>gg", function()
+set("n", "<leader>gg", function()
   local buf_path = vim.api.nvim_buf_get_name(0)
   if buf_path == "" then
     buf_path = vim.loop.cwd() -- fallback if buffer has no name
@@ -26,44 +27,56 @@ vim.keymap.set("n", "<leader>gg", function()
 end, { desc = "Lazygit (git root of buffer)" })
 
 -- LSP
-vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", { desc = "Code Action" })
-vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", { desc = "Goto Definition" })
-vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga peek_definition<cr>", { desc = "Sneak-peek of type definition" })
-vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<cr>", { desc = "Show References by search in directory" })
-vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
-vim.keymap.set("n", "<leader>sd", "<cmd>Lspsaga show_buf_diagnostics<cr>", { desc = "Show the diagnostic over buffer" })
-vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<cr>", { desc = "Line Diagnostic" })
-vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", { desc = "Hover" })
-vim.keymap.set("n", "<leader>cr", "<cmd>Lspsaga rename<cr>", { desc = "Rename" })
-vim.keymap.set("n", "]d", "<cmd>:lua require('lspsaga.diagnostic'):goto_next()<cr>", { desc = "Jump to the next diagnostic" })
-vim.keymap.set("n", "[d", "<cmd>:lua require('lspsaga.diagnostic'):goto_prev()<cr>", { desc = "Jump to the next diagnostic" })
+set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", { desc = "Code Action" })
+set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", { desc = "Goto Definition" })
+set("n", "<leader>pd", "<cmd>Lspsaga peek_definition<cr>", { desc = "Sneak-peek of type definition" })
+set("n", "gr", "<cmd>Lspsaga finder<cr>", { desc = "Show References by search in directory" })
+set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
+set("n", "<leader>sd", "<cmd>Lspsaga show_buf_diagnostics<cr>", { desc = "Show the diagnostic over buffer" })
+set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<cr>", { desc = "Line Diagnostic" })
+set("n", "K", "<cmd>Lspsaga hover_doc<cr>", { desc = "Hover" })
+set("n", "<leader>cr", "<cmd>Lspsaga rename<cr>", { desc = "Rename" })
+set("n", "]d", function()
+  require("lspsaga.diagnostic"):goto_next()
+end, { desc = "Jump to the next diagnostic" })
+set("n", "[d", function()
+  require("lspsaga.diagnostic"):goto_prev()
+end, { desc = "Jump to the prev diagnostic" })
 
 -- Windows stuff
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Delete others split window", remap = true, silent = true })
-vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-vim.keymap.set("n", "<leader>wm", "<cmd>FocusMaximise<cr>", { desc = "Maximize window for focus", remap = true })
+set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+set("n", "<leader>wo", "<C-w>o", { desc = "Delete others split window", remap = true, silent = true })
+set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+set("n", "<leader>wm", "<cmd>FocusMaximise<cr>", { desc = "Maximize window for focus", remap = true })
 
 -- find and replace
-vim.keymap.set("n", "<leader>sr", "<cmd>GrugFar<cr>", { desc = "Find and Replace" })
+set("n", "<leader>sr", "<cmd>GrugFar<cr>", { desc = "Find and Replace" })
 
 -- buffers
-vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-vim.keymap.set("n", "<leader>bd", "<cmd>:lua Snacks.bufdelete() <cr>", { desc = "Delete Buffer" })
-vim.keymap.set("n", "<leader>bo", "<cmd>:lua Snacks.bufdelete.other() <cr>", { desc = "Delete Other Buffers" })
-vim.keymap.set("n", "<leader>z", "<cmd>:lua Snacks.zen.zen() <cr>", { desc = "Zen Mode / Focus Mode" })
+set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+set("n", "<leader>bd", "<cmd>:lua Snacks.bufdelete() <cr>", { desc = "Delete Buffer" })
+set("n", "<leader>bo", "<cmd>:lua Snacks.bufdelete.other() <cr>", { desc = "Delete Other Buffers" })
+set("n", "<leader>z", "<cmd>:lua Snacks.zen.zen() <cr>", { desc = "Zen Mode / Focus Mode" })
 
 -- Clear search and stop snippet on escape
-vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
+set({ "i", "n", "s" }, "<esc>", function()
   vim.cmd("noh")
   vim.cmd("delm!")
   return "<esc>"
 end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+-- setup multi cursor
+set({ "n", "x" }, "<c-d>", function()
+  require("multicursor-nvim").matchAddCursor(1)
+end)
+set({ "n", "x" }, "<c>D", function()
+  require("multicursor-nvim").matchSkipCursor(1)
+end)
 
 require("config.keymaps-directory")
 require("config.keymaps-picker")
--- nvim/.config/nvim/lua/plugins/multicursor.nvim.lua
+++ nvim/.config/nvim/lua/plugins/multicursor.nvim.lua
@@ -4,47 +4,6 @@ return {
   config = function()
     local mc = require("multicursor-nvim")
     mc.setup()
-
-    -- local set = vim.keymap.set
-
-    -- Add or skip cursor above/below the main cursor.
-    -- set({ "n", "x" }, "<up>", function()
-    --   mc.lineAddCursor(-1)
-    -- end)
-    -- set({ "n", "x" }, "<down>", function()
-    --   mc.lineAddCursor(1)
-    -- end)
-    -- set({ "n", "x" }, "<leader><up>", function()
-    --   mc.lineSkipCursor(-1)
-    -- end)
-    -- set({ "n", "x" }, "<leader><down>", function()
-    --   mc.lineSkipCursor(1)
-    -- end)
-
-    -- Add or skip adding a new cursor by matching word/selection
-    -- set({ "n", "x" }, "<c-d>", function()
-    --   mc.matchAddCursor(1)
-    -- end)
-    -- set({ "n", "x" }, "<leader>s", function()
-    --   mc.matchSkipCursor(1)
-    -- end)
-    -- set({ "n", "x" }, "<leader>N", function()
-    --   mc.matchAddCursor(-1)
-    -- end)
-    -- set({ "n", "x" }, "<leader>S", function()
-    --   mc.matchSkipCursor(-1)
-    -- end)
-
-    -- Add and remove cursors with control + left click.
-    -- set("n", "<c-leftmouse>", mc.handleMouse)
-    -- set("n", "<c-leftdrag>", mc.handleMouseDrag)
-    -- set("n", "<c-leftrelease>", mc.handleMouseRelease)
-
-    -- Disable and enable cursors.
-    -- set({ "n", "x" }, "<c-q>", mc.toggleCursor)
-
-    -- Mappings defined in a keymap layer only apply when there are
-    -- multiple cursors. This lets you have overlapping mappings.
     mc.addKeymapLayer(function(layerSet)
       -- Select a different cursor as the main one.
       layerSet({ "n", "x" }, "<left>", mc.prevCursor)
